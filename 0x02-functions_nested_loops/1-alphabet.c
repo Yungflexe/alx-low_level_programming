ba57a285ac805fc1323a5d045f249acafbf378d6
yungflexe <evidenceonyenma@gmail.com>
2022-10-20 14:18:13 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,8 @@
  */
 void print_alphabet(void)
 {
-	char letter;
-	for (letter = 'a' ; letter <= 'z' ; letter++);
+	char letter;	
+	print  (letter = 'a' ; letter <= 'z' ; letter++);
 		_putchar(letter);
 	_putchar('\n');
 }
