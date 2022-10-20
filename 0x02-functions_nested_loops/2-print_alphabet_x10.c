ace89d6d2069dbd963891796a0a029d83e2d8890
yungflexe <evidenceonyenma@gmail.com>
2022-10-20 15:30:13 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -check the code
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void);
+{
+	int round = 0 ;
+	char letter = 'a';
+	
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			 _putchar(letter);
+			 letter++;
+		}
+
+		round++;
+	}
+}
