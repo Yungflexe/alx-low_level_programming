94eacdf6d7bba3712d95794837ed63f87419e508
yungflexe <evidenceonyenma@gmail.com>
2022-10-22 09:20:12 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print a sign of a number
+ * @n: n is a number
+ *
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		 _putchar('-');
+		return (_1);
+	}
+	else
+	{
+		_putchar (0);
+		return (0);
+	}
+}
