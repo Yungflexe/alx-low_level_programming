2b0ac28dfbe6612cecd713ca24ff3e581207c9bd
yungflexe <evidenceonyenma@gmail.com>
2022-10-23 02:38:07 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - print a sign of a number
  * @n: n is an integeri
  *
- * Return: 1 (if positive), 0 (if zero), -1 (if negative), 0 (if it less than zero)
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
  */
 
 int print_sign(int n)
