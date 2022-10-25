cf7a1e8f243d3a8cf96d2f580ceda5ea67a5347e
yungflexe <evidenceonyenma@gmail.com>
2022-10-25 15:40:47 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
  #include "main.h"
-
 /**
  * main - tests function that prints if integer is positive or negative
  *
@@ -8,8 +7,9 @@
 int main(void)
 {
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
 
-	return (0); 
+	return (0);
 }
